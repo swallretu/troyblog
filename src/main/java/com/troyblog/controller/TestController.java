528b1a556b3ecb02b3e4484ccb0269f06ca3ca1b
qujianfei <qujianfei@qujianfeideMacBook-Pro.local>
2015-12-21 23:14:55 +0800

    add spring denpendence and springmvc framework xml config and demo TestController

---
+++ src/main/java/com/troyblog/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.troyblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by qujianfei on 15/12/21.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(HttpServletRequest request, HttpServletResponse response){
+
+        System.out.println("come in controller");
+
+        return "index";
+    }
+
+}
